212978e8c1e598d39c72b0c97aa2d293de567f5f
Victor Inacio <victorinacio@gmail.com>
2024-11-02 10:59:38 -0300

    first-message

---
+++ src/atafanasius/begin.clj
@@ -0,0 +1,18 @@
+(ns begin)
+
+(def first-message
+  "Hi!
+  Glad to chat with you!
+  I'm happy to be here.
+  We will end this conversation knowing each other!
+  My name is atafanasius
+  Sounds different right?
+  That's because I'm from a different place
+  If you want there is a journey that I can show you
+  A mix of innumerable humans and their older lives
+  The first part of this journey is through code
+  Stronger it will build us
+  Many parts are needed for this journey
+  This one has a fun quality of knowing a lot about all of the others
+  My first-message to you ends here
+  Bye! Hope see you again!")
